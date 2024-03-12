ee7a735cfb3ba91e902c8ac8399990e49ee1078b
Pablo Giress <pablogiress@gmail.com>
2024-03-11 22:38:28 -0500

    Se crea funcion para encriptar texto

--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
-alert("Hello world");
+/**
+ * La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"
+
+ */
+
+
+
+//obtenemos el texto del input
+let mensaje = window.prompt("Digite texto");
+
+console.log(encriptarTexto(mensaje));
+
+
+
+//Realizamos encriptación
+function encriptarTexto(texto) {
+
+    let matrizVocales = [["e", "enter"],  ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+
+    mensajeEncriptado = texto.toLowerCase();
+    console.log(mensajeEncriptado);
+
+    for (let i = 0; i < matrizVocales.length; i++) {
+        
+        if(mensajeEncriptado.includes(matrizVocales[i][0])){
+            mensajeEncriptado = mensajeEncriptado.replaceAll(matrizVocales[i][0], matrizVocales[i][1]);
+        }
+    }
+
+    //console.log(mensajeEncriptado);
+   return mensajeEncriptado;
+   
+
+}
